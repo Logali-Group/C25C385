f71e096f49813a0d0f15ac955d8f2867b49dea6f
Jorge Luis BriceÃ±o Amesty <jbriceno@logaligroup.com>
2025-10-25 15:53:18 +0000

    DynamicPage - Update

--- employees/webapp/controller/Main.controller.ts
+++ employees/webapp/controller/Main.controller.ts
@@ -7,6 +7,9 @@ import Filter from "sap/ui/model/Filter";
 import Table from "sap/m/Table";
 import ListBinding from "sap/ui/model/ListBinding";
 import FilterOperator from "sap/ui/model/FilterOperator";
+// import * as XLSX from "xlsx";
+// import Binding from "sap/ui/model/Binding";
+// import Context from "sap/ui/model/Context";
 
 /**
  * @namespace com.logaligroup.employees.controller
@@ -63,4 +66,46 @@ export default class Main extends BaseController {
         combobox.setSelectedKey("");
         this.onFilterSearchPress(event);
     }
+
+    // public onExportToExcel(): void {
+            
+    //         // 1. Obtener la referencia a la tabla
+    //         const oTable = this.byId("table") as Table;
+
+    //         // 2. Obtener el binding de los items
+    //         // Esto es crucial porque nos da los datos YA FILTRADOS por el FilterBar
+    //         const oBinding: Binding = oTable.getBinding("items");
+    //         const aContexts: Context[] = oBinding.getContexts();
+
+    //         // 3. Obtener los objetos de datos puros de los contextos
+    //         const aTableData: any[] = aContexts.map((oContext: Context) => {
+    //             return oContext.getObject();
+    //         });
+
+    //         // 4. Transformar los datos al formato deseado para Excel
+    //         // Esto es importante porque tu ObjectIdentifier combina dos campos.
+    //         // Creamos un nuevo array de objetos con las cabeceras que queremos.
+    //         const aDataToExport = aTableData.map(oEmployee => {
+    //             return {
+    //                 "ID Empleado": oEmployee.EmployeeID,
+    //                 "Nombre Completo": `${oEmployee.LastName}, ${oEmployee.FirstName}`,
+    //                 "País": oEmployee.Country,
+    //                 "Ciudad": oEmployee.City,
+    //                 "Código Postal": oEmployee.PostalCode
+    //             };
+    //         });
+
+    //         // 5. Crear la Hoja de Cálculo (WorkSheet)
+    //         // Usamos 'json_to_sheet' que toma un array de objetos
+    //         const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(aDataToExport);
+
+    //         // 6. Crear el Libro de Trabajo (WorkBook)
+    //         const wb: XLSX.WorkBook = XLSX.utils.book_new();
+
+    //         // 7. Añadir la hoja al libro con un nombre (ej: "Empleados")
+    //         XLSX.utils.book_append_sheet(wb, ws, "Empleados");
+
+    //         // 8. Generar y descargar el archivo
+    //         XLSX.writeFile(wb, "ListaEmpleados.xlsx");
+    // }
 }
